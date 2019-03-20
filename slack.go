6f644b4abe21824ad75e8cbdaea0dd20983e98e9
Kotaro <7750koutarou@gmail.com>
2019-03-20 17:43:14 +0900

    debug message

--- slack.go
+++ slack.go
@@ -42,6 +42,7 @@ func (s *SlackListener) ListenAndResponse() {
 
 // handleMesageEvent handles message events.
 func (s *SlackListener) handleMessageEvent(ev *slack.MessageEvent) error {
+	log.Printf("%s %s", ev.Channel, ev.Msg.Text)
 	// Only response in specific channel. Ignore else.
 	if ev.Channel != s.channelID {
 		log.Printf("%s %s", ev.Channel, ev.Msg.Text)
